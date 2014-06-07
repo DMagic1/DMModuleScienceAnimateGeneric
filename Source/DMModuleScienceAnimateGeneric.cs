736299fdb02bc5a16432fb546119c554d0a46ce4
DMagic <david.grandy@gmail.com>
2014-06-07 17:06:47 -0400

    Hotfix for disappearing science reports

--- Source/DMModuleScienceAnimateGeneric.cs
+++ Source/DMModuleScienceAnimateGeneric.cs
@@ -161,6 +161,7 @@ namespace DMModuleScienceAnimateGeneric
                     }
                 }
             }
+            eventsCheck();
         }
 
         public override string GetInfo()
