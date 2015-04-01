e9d48e5b71152bf73e3e6d296d43074facd807bb
DMagic <david.grandy@gmail.com>
2015-04-01 13:33:43 -0400

    Version bump

--- Source/Properties/AssemblyInfo.cs
+++ Source/Properties/AssemblyInfo.cs
@@ -22,6 +22,6 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("148cb195-6345-4253-809b-f3d61338b45b")]
 
-[assembly: AssemblyVersion("0.6.0.0")]
-[assembly: AssemblyFileVersion("0.6.0.0")]
-[assembly: KSPAssembly("DMModuleScienceAnimateGeneric", 0, 6)]
+[assembly: AssemblyVersion("0.7.0.0")]
+[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: KSPAssembly("DMModuleScienceAnimateGeneric", 0, 7)]
